45edc9b77f18aabb664d22ba705e2a7741a6ce15
Rafael Pacheco <rafaelpache64@outlook.com>
2025-01-10 18:32:28 -0400

    Styling the comments and replies for post

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import { BACKEND_URL } from "../constants";
 import Header from "../components/Header";
 import { v4 as uuidv4 } from "uuid";
+import { useSelector } from "react-redux";
+import { RootState } from "../services/redux-toolkit/store";
+import ButtonComponent from "../components/ui/ButtonComponent";
 
 type postDataResponse = {
   authorId: string;
@@ -21,6 +24,7 @@ function PostPage() {
   const [selectedViewImage, setSelectedViewImage] = useState(0);
   const params = useParams();
   const postId = params.postId;
+  const user = useSelector((state: RootState) => state.auth.user);
 
   useEffect(() => {
     const getPost = async () => {
@@ -130,12 +134,106 @@ function PostPage() {
 
             <p className="text-lg text-center">{post.content}</p>
 
-            <p>
-              Tags:{" "}
-              {post.tags.length > 0
-                ? post.tags.join(", ")
-                : "there are no tags!"}
-            </p>
+            {post.tags.length > 0 ? (
+              <>
+                <p className="text-lg m-2">Tags: </p>
+
+                {post.tags.map((tag) => (
+                  <li
+                    key={uuidv4()}
+                    className="inline-block p-2 cursor-default text-lg mx-2 font-semibold bg-firstGreen rounded-full"
+                  >
+                    {tag}
+                  </li>
+                ))}
+              </>
+            ) : null}
+
+            <div className="flex gap-4 my-4">
+              <button className="p-2 bg-FirstDarkBlue text-firstGreen transition-all duration-150 hover:bg-firstGreen hover:text-white font-semibold">
+                Like
+                <i className=" mx-2 fa-solid fa-thumbs-up"></i>1
+              </button>
+
+              <button className="p-2 bg-firstBrown transition-all duration-150 hover:bg-firstGreen font-semibold">
+                Dislike
+                <i className=" mx-2 fa-solid fa-thumbs-down"></i>0
+              </button>
+            </div>
+          </section>
+
+          <div className="py-2 border-b-firstLavender border-b-4 border-t-firstLavender border-t-4 w-full bg-firstBrown"></div>
+
+          <section className="bg-mainBg text-white">
+            <h2 className="text-2xl font-semibold text-center">Comments</h2>
+
+            <form>
+              <div className="flex flex-col justify-start items-start w-[min(45rem,70%)]">
+                <label className="text-lg block" htmlFor="text">
+                  Write your comment:
+                </label>
+                <textarea
+                  className="block border-2 mt-2 w-full min-h-[4rem] lg:mt-0 lg:col-span-2 border-black resize-none p-2 text-black data-[error-input=true]:border-red-700"
+                  id="text"
+                  placeholder="Write text for your comment"
+                ></textarea>
+                <ButtonComponent
+                  typeButton="button"
+                  textBtn="Submit comment"
+                  additionalClassnames="self-end"
+                ></ButtonComponent>
+              </div>
+            </form>
+
+            <ul>
+              <li className="relative w-fit">
+                <div className="flex items-center">
+                  <img
+                    className="w-[min(7rem,7%)] h-[min(7rem,7%)] rounded-full"
+                    src={user.userData?.profilePictureURL}
+                  ></img>
+                  <p className="ml-2">{user.userData?.username}</p>
+                </div>
+                <p className="ml-[min(7rem,7%)]">
+                  Lore ipsum amet data adida lekelev achima derek anem malinda
+                  nemadi nomadi jalenimina
+                </p>
+                <div className="w-fit ml-[min(7rem,7%)]  flex gap-4 my-2">
+                  <button className="p-1 bg-firstBrown transition-all duration-150 hover:bg-firstGreen font-semibold">
+                    Like
+                    <i className=" mx-2 fa-solid fa-thumbs-up"></i>0
+                  </button>
+
+                  <button className="p-1 bg-firstBrown transition-all duration-150 hover:bg-firstGreen font-semibold">
+                    Dislike
+                    <i className=" mx-2 fa-solid fa-thumbs-down"></i>0
+                  </button>
+
+                  <button className="transition-all duration-150 hover:text-firstGreen font-semibold">
+                    Reply
+                    <i className="mx-2 fa-solid fa-reply"></i>
+                  </button>
+                </div>
+
+                <form className="ml-[min(7rem,7%)]">
+                  <div className="flex flex-col justify-start items-start w-[min(45rem,70%)]">
+                    <label className="text-lg block" htmlFor="text">
+                      Write your reply:
+                    </label>
+                    <textarea
+                      className="block border-1 mt-2 w-full min-h-[4rem] lg:mt-0 lg:col-span-2 border-black resize-none p-2 text-black data-[error-input=true]:border-red-700"
+                      id="text"
+                      placeholder="Write text for your reply"
+                    ></textarea>
+                    <ButtonComponent
+                      typeButton="button"
+                      textBtn="Submit reply"
+                      additionalClassnames="self-end p-1"
+                    ></ButtonComponent>
+                  </div>
+                </form>
+              </li>
+            </ul>
           </section>
         </>
       ) : (
