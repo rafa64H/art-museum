fa8c0d1fe6634d1bae9e60f7be0bb83651f360e6
Rafael Pacheco <rafaelpache64@outlook.com>
2025-03-24 13:15:55 -0400

    Removed old validation functions

--- backend/src/utils/form-input-validations/backendCheckValidityEmail.ts
+++
@@ -1,8 +0,0 @@
-export default function backendCheckValidityEmail(email: string) {
-  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!regexEmail.test(email)) {
-    return false;
-  }
-  return true;
-}
--- backend/src/utils/form-input-validations/backendCheckValidityPassword.ts
+++
@@ -1,9 +0,0 @@
-export default function backendCheckValidityPasswordBackend(
-  password: string
-): boolean {
-  const regexPassword = /(?=.*[a-zA-Z])(?=.*\d)(?=.*[^\w\s])/;
-  if (!regexPassword.test(password)) {
-    return false;
-  }
-  return true;
-}
