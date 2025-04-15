c0203687fc539476edc0da68a5c29d8458ea8ef0
Rafael Pacheco <rafaelpache64@outlook.com>
2025-04-15 16:19:45 -0400

    Added styles and also added setEmailVerified to verifyEmailPage.tsx

--- frontend/src/pages/VerifyEmailPage.tsx
+++ frontend/src/pages/VerifyEmailPage.tsx
@@ -1,22 +1,26 @@
 import { startTransition, useActionState, useEffect } from "react";
 import Header from "../components/Header";
-import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { Link, useParams } from "react-router-dom";
 import { isAxiosError } from "axios";
 import { verifyEmail } from "../utils/fetchFunctions";
+import { useDispatch } from "react-redux";
+import { setEmailVerified } from "../services/redux-toolkit/auth/authSlice";
 
 function VerifyEmailPage() {
   const params = useParams();
   const userIdParam = params.userId;
   const verifyEmailCode = params.code;
+  const dispatch = useDispatch();
   const [state, verifyEmailAction, isPending] = useActionState(async () => {
     try {
       await verifyEmail(userIdParam, verifyEmailCode);
-      return { success: "Verified email" };
+      dispatch(setEmailVerified(true));
+      return { success: "Your email has been verified" };
     } catch (error) {
       if (isAxiosError(error)) {
-        console.log(error.status, error.message);
-        return { error: "Error" };
+        if (error.status === 400)
+          return { error: "Error: The link is invalid" };
+        return { error: "Unexpected error, try again later" };
       }
     }
   }, null);
@@ -29,13 +33,21 @@ function VerifyEmailPage() {
     <>
       <Header></Header>
 
-      <h1 className="text-bold text-4xl text-red-400">
-        {!state || isPending
-          ? "Loading..."
-          : state && "error" in state
-          ? state.error
-          : state.success}
-      </h1>
+      <section className="text-white bg-firstLavender text-center py-40 px-20">
+        <h1 className="font-bold text-4xl">
+          {!state || isPending
+            ? "Loading..."
+            : state && "error" in state
+            ? `${state.error}`
+            : state.success}
+        </h1>
+
+        <p className="text-2xl mt-8">
+          <Link className="hover:underline" to={"/account-settings"}>
+            Go to account settings <i className="fa-solid fa-arrow-right"></i>
+          </Link>
+        </p>
+      </section>
     </>
   );
 }
